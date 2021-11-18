354f7cea89dfecbb90cc43d897dd07a23da82b4e
Faiza <faiza.fatema.1947@gmail.com>
2021-11-18 19:59:34 +0530

    added full project

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public Renderer meshRender;
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Vector2 ofset = meshRender.material.mainTextureOffset;
+        //ofset = ofset + new Vector2(0, speed * Time.deltaTime);
+        //meshRender.material.mainTextureOffset = ofset;
+
+        meshRender.material.mainTextureOffset += new Vector2(0, speed * Time.deltaTime);
+    }
+}
